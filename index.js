56b253a9af516d1765727489b33ce789be447470
Fareed86 <fa043541@gmail.com>
2021-10-23 12:32:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Wow I am excited to learn node with express and nodemone. automic restart');
+})
+
+const users = [
+    { id: 0, name: "Shabana", email: "shabana@gmail.com", phone: '01788888888' },
+    { id: 1, name: "Shabnoor", email: "Shabnoor@gmail.com", phone: '01788888888' },
+    { id: 2, name: "Shuchorita", email: "Shuchorita@gmail.com", phone: '01788888888' },
+    { id: 3, name: "Shrabonti", email: "Shrabonti@gmail.com", phone: '01788888888' },
+    { id: 4, name: "Soniya", email: "Soniya@gmail.com", phone: '01788888888' },
+    { id: 5, name: "Susmtia", email: "Susmtia@gmail.com", phone: '01788888888' }
+]
+
+app.get('/users', (req, res) => {
+    const search = (req.query.search);
+    // use query parameter
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users)
+    }
+})
+
+// app method
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('hitting the post', req.body);
+    res.send('inside post');
+    res.json(newUser)
+})
+
+// dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send['mango', 'oranges', 'banana', 'apple']
+})
+
+app.get('/fruits/mangoes/fazli', (req, res) => {
+    res.send('yummy yummy tok marka fazli');
+})
+
+app.listen(port, () => {
+    console.log('LISTENING TO PORT', port);
+})
